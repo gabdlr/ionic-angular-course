58f427c27b8ff8ac2a0187c8c3d6c86929d68036
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-22 22:13:44 -0300

    build(android): add capacitor android

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.angular',
+  appName: 'ionic-angular-course',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
