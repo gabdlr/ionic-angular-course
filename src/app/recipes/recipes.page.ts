cf02cd18f9f54e954a9822325faeb9bf64121c74
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-22 23:57:44 -0300

    fix: update list after when popping detail view

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipesService } from './recipes.service';
 import { Recipe } from './recipe.model';
+import { ViewWillEnter } from '@ionic/angular';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, ViewWillEnter {
   recipes: Recipe[];
   constructor(private recipesService: RecipesService) {
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+  ionViewWillEnter(): void {
     this.recipes = this.recipesService.getAllRecipes();
   }
   ngOnInit() {}
